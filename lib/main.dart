74e400ae23d9b3c7263a249df23091ad98f01297
ALEX DUTHIE <alexduthiemain@outlook.com>
2021-04-27 18:55:30 +0100

    Random Method to change 8ball image created

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  _AppState createState() => _AppState();
 }
 
+class _AppState extends State<App> {
+  int ballNum = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blue[300],
+          appBar: AppBar(
+            toolbarHeight: 80.0,
+            backgroundColor: Colors.blue[700],
+            title: Center(
+              child: Text(
+                'ASK ME ANYTHING...',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          body: Center(
+            child: TextButton(
+              onPressed: shakeBall,
+              child: Image(
+                image: AssetImage('images/ball$ballNum.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
